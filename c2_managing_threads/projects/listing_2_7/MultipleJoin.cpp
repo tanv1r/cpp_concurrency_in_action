276ff884abd788baa937789ca39e1b1d89a1d9c3
Mohammad Tanviruzzaman <tanvir.zaman@csebuet.org>
2020-06-12 00:02:59 -0700

    multi_join

---
+++ c2_managing_threads/projects/listing_2_7/MultipleJoin.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+void hello(int i)
+{
+    cout << "hello there " << i << endl;
+}
+
+int main()
+{
+    vector<thread> mythreads;
+    for (unsigned i = 0; i < 10; ++i)
+    {
+        mythreads.push_back( thread{hello, i} );
+    }
+
+    for_each( mythreads.begin(), mythreads.end(), mem_fn(&thread::join) );
+}
