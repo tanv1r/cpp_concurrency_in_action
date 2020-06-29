99d1c6b9eabf576bab7cf75cd566553ae9f2e2b2
Mohammad Tanviruzzaman <tanvir.zaman@csebuet.org>
2020-06-28 23:02:33 -0700

    promise_simple_example

---
+++ c4_synchronize_concurrent_operations/projects/listing_4_10/PromiseExample.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <functional>
+#include <thread>
+#include <future>
+
+using namespace std;
+
+void printInteger(future<int> & fut)
+{
+    int x = fut.get();
+    cout << "value: " << x << endl;
+}
+
+int main()
+{
+    promise<int> prom;
+    auto fut = prom.get_future();
+
+    thread t1(printInteger, ref(fut));
+
+    prom.set_value(100);
+
+    t1.join();
+
+    return 0;
+}
